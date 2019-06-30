ef610c039aac69c635f417c4de53afba37885d1a
erugis0345 <tl091264@gmail.com>
2019-06-30 16:31:19 +0900

    userモデルとのリレーション

--- todo/models.py
+++ todo/models.py
@@ -1,5 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Task(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
     name  = models.CharField(max_length=100)
     state = models.CharField(max_length=20)
